a3e2fc45ce353dd7032f1b161af8016eeee415c4
Mussa Mipawa <mepowerleo10@gmail.com>
2022-04-15 14:19:59 +0300

    feat: added Author model

--- catalog/models.py
+++ catalog/models.py
@@ -80,3 +80,21 @@ class BookInstance(models.Model):
     def __str__(self):
         """String representation of the Book instance"""
         return f"{self.id} ({self.book.title})"
+
+
+class Author(models.Model):
+    """A model representing a book Author"""
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def get_absolute_url(self):
+        return reverse("author-detail", args=(str(self.id)))
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
