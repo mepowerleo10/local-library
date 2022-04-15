2810eecdc3b913fe5fae3d62dcdb5d9e9c867c32
Mussa Mipawa <mepowerleo10@gmail.com>
2022-04-15 10:51:01 +0300

    feat: added the Book model

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Genre(models.Model):
@@ -11,3 +12,35 @@ class Genre(models.Model):
     def __str__(self):
         """String representation of the Genre object"""
         return self.name
+
+
+class Book(models.Model):
+    """Model representing a single Book (not a specific copy)"""
+
+    title = models.CharField(max_length=200)
+
+    # using a Foreign key because the book can can only have one author,
+    # but authors can have multiple books
+    # # Author as a string rather that a model because it hasn't been declared yet in the file
+    author = models.ForeignKey("Author", null=True, on_delete=models.SET_NULL)
+    summary = models.TextField(
+        blank=True, max_length=1000, help_text="Enter a brief description of the book"
+    )
+    isbn = models.CharField(
+        "ISBN",
+        max_length=13,
+        unique=True,
+        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>',
+    )
+
+    # ManyToManyField used because a genre can contain many books. And books can cover many genres.
+    # Genre class has already been defined so we can specify the class above
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        """String representing the Book object"""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access the detail of the record."""
+        return reverse("book-detail", args=[str(self.id)])
