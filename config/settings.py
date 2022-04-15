348d15fa009a1c84c6a2f9e1d70192402d5fd2a5
Mussa Mipawa <mepowerleo10@gmail.com>
2022-04-15 09:30:53 +0300

    feat: added static files directory

--- config/settings.py
+++ config/settings.py
@@ -100,6 +100,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
