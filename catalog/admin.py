e1f211093e1050a2af07c9db6848b3a3a96397e6
Mussa Mipawa <mepowerleo10@gmail.com>
2022-04-17 04:29:22 +0300

    refactor: moved all models to class based admin views

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
-registered_models = [Author, Genre, Book, BookInstance, Language]
-admin.site.register(registered_models)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
