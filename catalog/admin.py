464a4bbce1228d4cf16469b2b565f4ae01a71873
Mussa Mipawa <mepowerleo10@gmail.com>
2022-04-17 05:14:06 +0300

    refactor: added inline methods for Book and BookInstance objects in Author and Book respectively

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,19 +2,44 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["last_name", "first_name", ("date_of_birth", "date_of_death")]
+
+    inlines = [BookInline]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "author", "display_genre")
+
+    inlines = [BookInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ("status", "due_back")
+    list_display = ("book", "status", "due_back", "id")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("book", "imprint", "id"),
+            },
+        ),
+        ("Availability", {"fields": ("status", "due_back")}),
+    )
 
 
 @admin.register(Genre)
